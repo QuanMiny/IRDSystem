bd14f4a314582865c3d053c6ac4cadd7aaa6910e
Miny <944307891@qq.com>
2024-04-24 00:01:24 +0800

    接口代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, UserConfig, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -6,23 +6,36 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ]
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const viteEnv = loadEnv(mode, process.cwd())
+  console.log(viteEnv)
+  return {
+    base: './',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: viteEnv.VITE_PORT as unknown as number,
+      open: viteEnv.VITE_OPEN,
+      proxy: {
+        '/api': {
+          target: viteEnv.VITE_PROXY,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
 })
