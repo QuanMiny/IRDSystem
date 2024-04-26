edd1eddb961a7c36639e18cfc29835b5cea5dbf9
Miny <944307891@qq.com>
2024-04-26 11:08:38 +0800

    el-message样式

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import '@/styles/reset.scss'
 // 公共样式
 import '@/styles/common.scss'
 
+// el-message 提示框样式
+import 'element-plus/es/components/message-box/style/css'
+import 'element-plus/es/components/message/style/css'
+
 import router from '@/routers'
 
 import pinia from '@/stores'
