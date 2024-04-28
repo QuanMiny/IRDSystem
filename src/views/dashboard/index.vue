fec0e0e63e76e279c2b90f3b265b9bce208be4fd
Miny <944307891@qq.com>
2024-04-28 22:56:44 +0800

    滚动条

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="dashboard">
-    主面板
-    <router-link to="/system/menuManage">跳跳</router-link>
+  <div class="card">
+    <div style="height: 800px"></div>
   </div>
 </template>
 
