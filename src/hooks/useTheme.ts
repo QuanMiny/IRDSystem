69a44581fcef84a997502d545c1d243ab8278db1
Miny <944307891@qq.com>
2024-04-30 23:46:40 +0800

    主题色

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,6 @@
 import { DEFAULT_PRIMARY } from '@/config'
 import { useGlobalStore } from '@/stores/modules/global'
+import { getLightColor } from '@/utils/color'
 
 export const useTheme = () => {
   const globalStore = useGlobalStore()
@@ -10,6 +11,10 @@ export const useTheme = () => {
       val = DEFAULT_PRIMARY
     }
     document.documentElement.style.setProperty('--el-color-primary', val)
+    for (let i = 1; i <= 9; i++) {
+      const primaryColor = `${getLightColor(val, i / 10)}`
+      document.documentElement.style.setProperty(`--el-color-primary-light-${i}`, primaryColor)
+    }
     globalStore.setPrimaryState(val)
   }
 
